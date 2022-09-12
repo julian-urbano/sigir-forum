cf300f98f7b24c5df3509e8e2deb70870e1a4f51
julian-urbano <urbano.julian@gmail.com>
2022-09-12 23:41:33 +0200

    Add quick build script to build zip releases.

---
+++ template/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cp -f sample.tex zip/samples/
+cp -f sample.pdf zip/samples/
+cp -f sample_event.tex zip/samples/
+cp -f sample_event.pdf zip/samples/
+cp -f sample_phd.tex zip/samples/
+cp -f sample_phd.pdf zip/samples/
+
+cp -f checklist.pdf zip/
+cp -f instructions.pdf zip/
+cp -f sigirforum.bib zip/
+cp -f sigirforum.cls zip/
+
+echo 'Version? '
+read version
+7z a releases/template-${version}.zip ./zip/*
